74f648bd6411864634969342df884ebb0dff5b61
Ahmad Daly <a.dalyomar@magenus.com>
2023-05-01 19:51:43 +0300

    rewritten the extraction algorithm to extract all the localizations at once

--- lib/main.dart
+++ lib/main.dart
@@ -7,15 +7,29 @@ void main() async {
   var bytes = File(file).readAsBytesSync();
   var excel = Excel.decodeBytes(bytes);
 
+  /// loop tables in files
+  /// currently only 1 table is present in file
   for (var table in excel.tables.keys) {
-    Map<String, String> localeMap = {};
+    List<Map<String, String>> allTranslations =
+        List.generate(excel.tables[table]!.rows[0].length - 1, (index) => {});
+
+    /// loop on all excel rows
     for (var row in excel.tables[table]!.rows) {
       String key = row[0]!.value.toString();
-      String englishValue = row[1]!.value.toString();
-      localeMap.addAll({key: englishValue});
+
+      /// loop on row columns
+      /// loads translation values
+      for (int columnIndex = 1; columnIndex < row.length; columnIndex++) {
+        String translationValue = row[columnIndex]!.value.toString();
+        allTranslations[columnIndex - 1].addAll({key: translationValue});
+      }
     }
 
-    String file = "assets/english.json";
-    File(file).writeAsStringSync(jsonEncode(localeMap));
+    /// export result maps to localization files
+    for (var languageMap in allTranslations) {
+      String languageCode = languageMap['language_code'] ?? 'unknown';
+      String file = "assets/$languageCode.json";
+      File(file).writeAsStringSync(jsonEncode(languageMap));
+    }
   }
 }
