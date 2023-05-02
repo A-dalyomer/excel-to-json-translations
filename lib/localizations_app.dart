9fa7f852e8af08fef25d22b5b6168b4af1b20775
Ahmad Daly <a.dalyomar@magenus.com>
2023-05-02 16:38:04 +0300

    added app title

--- lib/localizations_app.dart
+++ lib/localizations_app.dart
@@ -8,6 +8,7 @@ class LocalizationsApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      title: 'Localizations App',
       home: HomeScreen(),
     );
   }
