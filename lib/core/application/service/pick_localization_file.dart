f94b4c5771cdae731660b84d143b060408c45386
Ahmad Daly <a.dalyomar@magenus.com>
2023-05-03 22:00:21 +0300

    fixed windows app file pick

--- lib/core/application/service/pick_localization_file.dart
+++ lib/core/application/service/pick_localization_file.dart
@@ -1,11 +1,14 @@
-import 'dart:typed_data';
-
+import 'dart:io';
 import 'package:file_picker/file_picker.dart';
+import 'package:flutter/foundation.dart';
 
 Future<Uint8List?> pickLocalizationsFile() async {
   FilePickerResult? result = await FilePicker.platform.pickFiles();
 
   if (result != null) {
+    if (!kIsWeb) {
+      return File(result.files.single.path!).readAsBytesSync();
+    }
     return result.files.single.bytes;
   } else {
     return null;
